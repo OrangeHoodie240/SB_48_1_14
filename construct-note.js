00daa1511c1c28bd436f71f1c44432d6f29b0c2e
steven daddario <daddarios@dupage.edu>
2021-10-19 04:33:09 -0500

    testing

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+    if(message.length > letters.length) return false;
+    const messageCount = count(message);
+    const lettersCount = count(letters); 
+    for(let key in messageCount){
+        if(messageCount[key] !== lettersCount[key]) return false; 
+    }
+    return true; 
+}
+
+
+function count(collection){
+    if(typeof collection === 'string'){
+        collection = collection.split('');
+    }
+
+    return collection.reduce((a,b)=>{
+        a[b] = (a[b] || 0) + 1; 
+        return a; 
+    }, {});
 }
